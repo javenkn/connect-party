9028fb4a140fc3276c56b42d24df34fd53f34336
javenkn <javenkn7@gmail.com>
2020-04-08 11:04:46 -0700

    Adds mutations for logging in and registering

--- server/src/UserResolver.ts
+++ server/src/UserResolver.ts
@@ -1,9 +1,68 @@
-import { Resolver, Query } from "type-graphql";
+import {
+  Resolver,
+  Query,
+  Mutation,
+  Arg,
+  ObjectType,
+  Field,
+} from "type-graphql";
+import { hash, compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
+import { User } from "./entity/User";
+
+@ObjectType()
+class LoginResponse {
+  @Field()
+  accessToken: string;
+}
 
 @Resolver()
 export class UserResolver {
   @Query(() => String)
   hello() {
     return "hi";
+  }
+
+  @Query(() => [User])
+  users() {
+    return User.find();
+  }
+
+  @Mutation(() => LoginResponse)
+  async login(
+    @Arg("email") email: string,
+    @Arg("password") password: string
+  ): Promise<LoginResponse> {
+    const user = await User.findOne({ where: { email } });
+
+    if (!user) {
+      throw new Error("Could not find user.");
+    }
+
+    const valid = await compare(password, user.password);
+
+    if (!valid) {
+      throw new Error("Incorrect password.");
+    }
+
+    //login successful
+    return {
+      accessToken: sign({ userId: user.id }, "secret", { expiresIn: "15m" }),
+    };
+  }
+
+  @Mutation(() => Boolean)
+  async register(
+    @Arg("email") email: string,
+    @Arg("password") password: string
+  ) {
+    const hashedPassword = await hash(password, 12);
+    try {
+      await User.insert({ email, password: hashedPassword });
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+    return true;
   }
 }
