492a027d3487fe08fa93e3ed7896aff24660f0d2
javenkn <javenkn7@gmail.com>
2020-04-08 16:34:52 -0700

    Creates routes for navigating through the app

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "@apollo/react-hooks";
-import App from "./App";
+import Routes from "./Routes";
 
 const client = new ApolloClient({
   uri: "http://localhost:4000/graphql",
@@ -11,7 +11,7 @@ const client = new ApolloClient({
 ReactDOM.render(
   <ApolloProvider client={client}>
     <React.StrictMode>
-      <App />
+      <Routes />
     </React.StrictMode>
   </ApolloProvider>,
   document.getElementById("root")
