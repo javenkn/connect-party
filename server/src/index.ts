4425d84661180d66ba1812783300df7071aafbbd
javenkn <javenkn7@gmail.com>
2020-04-08 11:04:29 -0700

    Adds connection creation for typeorm

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,16 @@
 import express from "express";
 import { ApolloServer } from "apollo-server-express";
-import "reflect-metadata";
 import { buildSchema } from "type-graphql";
+import { createConnection } from "typeorm";
+import "reflect-metadata";
 import { UserResolver } from "./UserResolver";
 
 (async () => {
   const app = express();
 
   app.get("/", (_req, res) => res.send("hello world"));
+
+  await createConnection();
 
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
@@ -21,21 +24,3 @@ import { UserResolver } from "./UserResolver";
     console.log("Listening on port 4000");
   });
 })();
-// createConnection()
-//   .then(async (connection) => {
-//     console.log("Inserting a new user into the database...");
-//     const user = new User();
-//     user.firstName = "Timber";
-//     user.lastName = "Saw";
-//     user.age = 25;
-//     console.log(connection);
-//     await connection.manager.save(user);
-//     console.log("Saved a new user with id: " + user.id);
-
-//     console.log("Loading users from the database...");
-//     const users = await connection.manager.find(User);
-//     console.log("Loaded users: ", users);
-
-//     console.log("Here you can setup and run express/koa/any other framework.");
-//   })
-//   .catch((error) => console.log(error));
