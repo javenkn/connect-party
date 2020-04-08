7c62e5972fbfbbf0ac25a9206bc22b5b264ba53d
javenkn <javenkn7@gmail.com>
2020-04-08 12:21:59 -0700

    Creates refresh token route and logic

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,14 +2,45 @@ import express from "express";
 import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
 import { createConnection } from "typeorm";
+import cookieParser from "cookie-parser";
 import "reflect-metadata";
 import "dotenv/config";
 import { UserResolver } from "./UserResolver";
+import { verify } from "jsonwebtoken";
+import { User } from "./entity/User";
+import { createAccessToken, createRefreshToken } from "./auth";
+import { sendRefreshToken } from "./sendRefreshToken";
 
 (async () => {
   const app = express();
 
+  app.use(cookieParser());
   app.get("/", (_req, res) => res.send("hello world"));
+
+  app.post("/refresh_token", async (req, res) => {
+    const token = req.cookies.jid;
+    if (!token) {
+      return res.send({ ok: false, accessToken: "" });
+    }
+
+    let payload: any = null;
+    try {
+      payload = verify(token, process.env.REFRESH_TOKEN_SECRET!);
+    } catch (error) {
+      console.log(error);
+      return res.send({ ok: false, accessToken: "" });
+    }
+
+    // token is valid and we can send another access token
+    const user = await User.findOne({ id: payload.userId });
+    if (!user) {
+      return res.send({ ok: false, accessToken: "" });
+    }
+
+    sendRefreshToken(res, createRefreshToken(user));
+
+    return res.send({ ok: true, accessToken: createAccessToken(user) });
+  });
 
   await createConnection();
 
