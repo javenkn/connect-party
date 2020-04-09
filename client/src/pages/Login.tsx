558cfa1b68a12283919fcbbc98c1de6a881ba798
javenkn <javenkn7@gmail.com>
2020-04-09 11:43:51 -0700

    Adds login query and logic

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,7 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+import { RouteComponentProps } from "react-router-dom";
+import { useLoginMutation } from "../generated/graphql";
 
 export type LoginProps = {};
 
-export const Login: React.FC<LoginProps> = () => <div>LOGIN</div>;
+export const Login: React.FC<RouteComponentProps> = ({ history }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [login] = useLoginMutation();
+
+  return (
+    <form
+      onSubmit={async (e) => {
+        e.preventDefault();
+        console.log("Form submitted");
+        const response = await login({
+          variables: {
+            email,
+            password,
+          },
+        });
+
+        console.log(response);
+        history.push("/");
+      }}
+    >
+      LOGIN
+      <input
+        value={email}
+        placeholder="email"
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        value={password}
+        placeholder="password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button type="submit">Login</button>
+    </form>
+  );
+};
 
 export default Login;
