e52b71f31b61f40da98369a83276f302452ace47
javenkn <javenkn7@gmail.com>
2020-04-09 15:46:39 -0700

    Handles logic for persisting access token on refresh

---
+++ client/src/App.tsx
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from "react";
+import Routes from "./Routes";
+import { setAccessToken } from "./accessToken";
+
+const App: React.FC = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/refresh_token", {
+      method: "POST",
+      credentials: "include",
+    }).then(async (res) => {
+      const { accessToken } = await res.json();
+      setAccessToken(accessToken);
+      setLoading(false);
+    });
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  return <Routes />;
+};
+
+export default App;
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "@apollo/react-hooks";
-import Routes from "./Routes";
 import { getAccessToken } from "./accessToken";
+import App from "./App";
 
 const client = new ApolloClient({
   uri: "http://localhost:4000/graphql",
@@ -23,7 +23,7 @@ const client = new ApolloClient({
 ReactDOM.render(
   <ApolloProvider client={client}>
     <React.StrictMode>
-      <Routes />
+      <App />
     </React.StrictMode>
   </ApolloProvider>,
   document.getElementById("root")
